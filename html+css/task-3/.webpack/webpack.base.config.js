463cedb37c806bd39612556fed1054a8f3f1fa17
Taras Barabash <tarasbarabash@gmail.com>
2019-11-22 19:14:06 +0200

    Task 3 (HTML + CSS)

---
+++ html+css/task-3/.webpack/webpack.base.config.js
@@ -0,0 +1,110 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const WebpackManifestPlugin = require("webpack-pwa-manifest");
+
+const APP_NAME = "Claire Foy";
+const SHORT_NAME = "Foy-Task-3";
+const APP_DESCRITION = "Claire Foy Demo Project";
+
+const PATHS = {
+  src: path.join(__dirname, "../src"),
+  build: path.join(__dirname, "../build"),
+  assets: "/assets"
+};
+
+module.exports = {
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    app: PATHS.src
+  },
+  output: {
+    filename: `js/[name].[hash].js`,
+    path: PATHS.build,
+    publicPath: ""
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          name: "vendor",
+          test: /node_modules/,
+          enforce: true,
+          chunks: "all"
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/"
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]"
+        }
+      },
+      {
+        test: /\.(c|sc)ss$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader?sourceMap",
+          {
+            loader: "postcss-loader",
+            options: {
+              config: {
+                path: "./.webpack/postcss.config.js"
+              },
+              sourceMap: true
+            }
+          },
+          "sass-loader?sourceMap"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `css/[name].[hash].css`
+    }),
+    new MinifyPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${PATHS.src}/index.html`,
+      filename: "index.html",
+      inject: true
+    }),
+    new CopyWebpackPlugin([
+      { from: `${PATHS.src}/assets`, to: `${PATHS.build}/assets` }
+    ]),
+    new WebpackManifestPlugin({
+      name: APP_NAME,
+      short_name: SHORT_NAME,
+      description: APP_DESCRITION,
+      background_color: "#fff",
+      theme_color: "#fff",
+      display: "standalone",
+      start_url: "index.html",
+      icons: [
+        {
+          src: path.resolve(`${PATHS.src}/assets/icons/icon.png`),
+          sizes: [72, 128, 152, 384, 512]
+          // destination: `${PATHS.assets}`
+        }
+      ]
+    }),
+    new InjectManifest({
+      swSrc: `${PATHS.src}/sw.js`
+    })
+  ]
+};
---
+++ html+css/task-3/src/js/index.js
@@ -0,0 +1,28 @@
+const showTemplateBtn = document.querySelector("#showTemplate");
+
+showTemplateBtn.addEventListener("click", () => {
+  const template = document.querySelector("template");
+  document
+    .querySelector("#template")
+    .appendChild(template.content.cloneNode(true));
+  showTemplateBtn.disabled = true;
+});
+
+document.querySelector("#confirm").addEventListener("click", () => {
+  document.querySelector("form").submit();
+});
+document.querySelector("#deny").addEventListener("click", () => {
+  document.querySelector("dialog").style.transform = "scale(0)";
+  document.querySelector("dialog").style.visibility = "hidden";
+});
+
+document.querySelector("#send-feedback").addEventListener("click", e => {
+  if (!document.querySelector("form").checkValidity()) return false;
+  e.preventDefault();
+  document.querySelector("dialog").style.transform = "scale(1)";
+  document.querySelector("dialog").style.visibility = "visible";
+});
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+ctx.fillRect(20, 20, 20, 20);
